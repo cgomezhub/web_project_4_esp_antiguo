73af653c0f973d87b5374df71ac38b0fd0a7e05a
Carlos Gomez <cgomezlugo@hotmail.com>
2023-08-03 12:55:35 -0400

    hotfixes: change java

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-let btnAbrir = document.querySelector(".button-perfil");
-let ventaEmergente = document.querySelector(".popup");
-let btnCerrar = document.querySelector(".popup__close");
 
-btnAbrir.addEventListener('click', () => { ventaEmergente.style.display = 'block'; });
-btnCerrar.addEventListener('click', () => { ventaEmergente.style.display = 'none';}); 
+let editButton = document.querySelector(".button-perfil");
+let popup = document.querySelector(".popup");
+let closeButton = document.querySelector(".popup__close");
+let saveButton = document.querySelector(".popup__guardar");
+let nameInput = document.querySelector(".popup__name");
+let jobInput = document.querySelector(".popup__acerca");
+let nombre = document.querySelector(".profile__nombre");
+let job = document.querySelector(".profile__acerca");
+
+editButton.addEventListener('click', function() {
+    nameInput.value = nombre.textContent;
+    jobInput.value = job.textContent;
+    popup.style.display = 'block';
+});
+closeButton.addEventListener('click', function() { popup.style.display = 'none';}); 
 
 let formElement = document.querySelector(".popup__container");
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
+    let nameDisplay = document.querySelector(".profile__nombre"); 
+    let jobDisplay = document.querySelector(".profile__acerca");
     let nameInput = document.querySelector(".popup__name");
     let jobInput = document.querySelector(".popup__acerca");
     let nameValue = nameInput.value; 
     let jobValue = jobInput.value; 
-    let nameDisplay = document.querySelector(".profile__nombre"); 
-    let jobDisplay = document.querySelector(".profile__acerca");
-    jobDisplay.textContent = jobValue;
     nameDisplay.textContent = nameValue;
+    jobDisplay.textContent = jobValue;
+    
 }
 formElement.addEventListener('submit', handleProfileFormSubmit);
+
+
+
 
 let heartImage = document.querySelectorAll(".element__heart");
 heartImage.forEach((boton) =>{boton.addEventListener("click", () => {
  if (boton.src.includes("images/heart.svg")){boton.src = "images/Black-heart.png";} 
  else {boton.src = "images/heart.svg"; } });});
 
-
+let btnAdd = document.querySelector(".button-lugar");
+let ventaModal = document.querySelector(".popup-add");
+let btnClose = document.querySelector(".popup-add__close");
+btnAdd.addEventListener('click', () => { ventaModal.style.display = 'block'; });
+btnClose.addEventListener('click', () => { ventaModal.style.display = 'none';}); 
+ 
